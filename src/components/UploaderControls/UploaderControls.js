e01f9622f3b0801eedd02159a87bb19bd0940e99
Gavin Quirk <gavinquirk@gmail.com>
2019-02-09 13:39:56 -0800

    toggle between enabled and disabled questions

--- src/components/UploaderControls/UploaderControls.js
+++ src/components/UploaderControls/UploaderControls.js
@@ -14,37 +14,52 @@ const questions = [
 class UploaderControls extends Component {
 
   state = {
-    selectedQuestions: questions
+    enabledQuestions: questions,
+    disabledQuestions: ["test"]
   }
 
   toggleSelectionHandler = (event) => {
     // TODO: Should this be done immutably?
-    const oldArray = this.state.selectedQuestions
+    const oldEnabledQuestions = this.state.enabledQuestions
+    const oldDisabledQuestions = this.state.disabledQuestions
 
-    // If the array already contains the selection
-    if (this.state.selectedQuestions.includes(event.target.innerText)) {
-      // Remove it from the array
-      var newArray = oldArray.filter(e => e !== event.target.innerText)
-      this.setState({ selectedQuestions: newArray})
-    } else {
-      // If not, add it to the array
-      const newArray = oldArray.concat(event.target.innerText)
-      this.setState({ selectedQuestions: newArray})
+    // If the enabled array already contains the selection
+    if (this.state.enabledQuestions.includes(event.target.innerText)) {
+      // Remove it from the enabled array
+      var newEnabledQuestions = oldEnabledQuestions.filter(e => e !== event.target.innerText)
+      // Add it to the disabled array
+      var newDisabledQuestions = oldDisabledQuestions.concat(event.target.innerText)
+      // Update State
+      this.setState({ enabledQuestions: newEnabledQuestions, disabledQuestions: newDisabledQuestions})
+
+    } else if (this.state.disabledQuestions.includes(event.target.innerText)) {
+      const newDisabledQuestions = oldDisabledQuestions.filter(e => e !== event.target.innerText)
+      const newEnabledQuestions = oldEnabledQuestions.concat(event.target.innerText)
+      this.setState({ enabledQuestions: newEnabledQuestions, disabledQuestions: newDisabledQuestions})
     }
   }
 
   render () {
 
-    const questionsDisplay = this.state.selectedQuestions.map(question =>
-      <p onClick={this.toggleSelectionHandler}>{question.valueOf()}</p>
+    const enabledQuestions = this.state.enabledQuestions.map(question =>
+      <button className={classes.questionBtn} onClick={this.toggleSelectionHandler}>{question.valueOf()}</button>
+    )
+
+    const disabledQuestions = this.state.disabledQuestions.map(question =>
+      <button className={classes.questionBtn} onClick={this.toggleSelectionHandler}>{question.valueOf()}</button>
     )
 
 
     return (
       <div className={classes.UploaderControls}>
-        <h3 style={{textAlign: 'center'}}>Choose what you would like to ask</h3>
-        {questionsDisplay}
-        <button onClick={this.props.postPersonHandler}>TEST SUBMIT</button>
+        {/* <h3 style={{textAlign: 'center'}}>Choose what you would like to ask</h3> */}
+        <div className={classes.enabledQuestions}>
+          {enabledQuestions}
+        </div>
+        <div className={classes.disabledQuestions}>
+          {disabledQuestions}
+        </div>
+        {/* <button onClick={this.props.postPersonHandler}>TEST SUBMIT</button> */}
       </div>
     )
 
