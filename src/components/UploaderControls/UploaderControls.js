3d1d08b0664ebd18ec07f6b9148777e8ece92944
Gavin Quirk <gavinquirk@gmail.com>
2019-02-08 16:36:03 -0800

    toggle selection handler

--- src/components/UploaderControls/UploaderControls.js
+++ src/components/UploaderControls/UploaderControls.js
@@ -3,19 +3,47 @@ import classes from './UploaderControls.module.css';
 
 // Will receive imageURL from props
 
+// Global Array of Questions
+const questions = [
+  "What is this persons name?",
+  "What is this persons age?",
+  "What is this persons occupation?",
+  "Would you be friends with this person?"
+]
+
 class UploaderControls extends Component {
+
+  state = {
+    selectedQuestions: questions
+  }
+
+  toggleSelectionHandler = (event) => {
+    // TODO: Should this be done immutably?
+    const oldArray = this.state.selectedQuestions
+
+    // If the array already contains the selection
+    if (this.state.selectedQuestions.includes(event.target.innerText)) {
+      // Remove it from the array
+      var newArray = oldArray.filter(e => e !== event.target.innerText)
+      this.setState({ selectedQuestions: newArray})
+    } else {
+      // If not, add it to the array
+      const newArray = oldArray.concat(event.target.innerText)
+      this.setState({ selectedQuestions: newArray})
+    }
+  }
 
   render () {
 
-    let questionSelectors = []
+    const questionsDisplay = this.state.selectedQuestions.map(question =>
+      <p onClick={this.toggleSelectionHandler}>{question.valueOf()}</p>
+    )
+
 
     return (
       <div className={classes.UploaderControls}>
         <h3 style={{textAlign: 'center'}}>Choose what you would like to ask</h3>
-        <p>What is this persons name?</p>
-        <p>What is this persons age?</p>
-        <p>What is this persons occupation?</p>
-        <p>Would you be friends with this person?</p>
+        {questionsDisplay}
         <button onClick={this.props.postPersonHandler}>TEST SUBMIT</button>
       </div>
     )
