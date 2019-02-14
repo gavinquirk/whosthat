d3094740106bf7a9e9c96125dd76566d2a4c3cfb
Gavin Quirk <gavinquirk@gmail.com>
2019-02-14 15:27:10 -0800

    Auth component, Input component and controls, Button component

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,54 @@
+// Sign up sign in form
+
+import React, { Component } from 'react';
+
+class Auth extends Component {
+
+  state = {
+    controls: {
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your Email Address'
+        },
+        value: '',
+        validation: {
+          required: true,
+          isEmail: true
+        },
+        valid: false,
+        touched: false
+      },
+      password: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'password',
+          placeholder: 'Your Password'
+        },
+        value: '',
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false,
+        touched: false
+      }
+    }
+  }
+
+
+  render () {
+    return (
+      <div>
+        <form action="">
+        
+        </form>
+      </div>
+    )
+  }
+
+
+}
+
+export default Auth;
